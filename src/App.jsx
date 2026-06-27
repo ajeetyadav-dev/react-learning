869a78c614c5089e3240f7196143fa2bfb6343dd
https://github.com/ajeetyadav0 <ay605403@gmail.com>
2026-06-27 20:57:28 +0530

    27

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
 import { useState } from "react";
 function App(){
- const [fruits,setFruits] = useState(["Apple","Mango"]);
- return(
-  <>
+    const [count,setCount] = useState(0);
+    return(
+        <>     
+           <h1>Counter : {count}</h1>
+        <button 
+        onClick={()=>
+                setCount(count + 1)}
+            
+        >+</button>
+        <button onClick={() =>{
+            if (count > 0){
+                setCount(count - 1);
+            }
+        }}>-</button>
+        <button onClick={() =>setCount(0)}>Reset</button>
+        <button onClick={() => setCount(count + 5)}>+5</button>
+        <hr></hr>
+        {count >=10 ? (
+            <h2 style={{ color : "green"}}>🎉Great Job!</h2>):(
+                <h2 style={{ color : "orange"}}>Keep Clicking.....</h2>
+            
+        )}
+           </>
+       
 
-   <ul>
-    {fruits.map((fruit,i)=>(
-        <li key={i}>{fruit}</li>
-      
-    ))}
-   </ul>
-  </>
-   
- 
-  
-
- );
+    );
 }
 export default App;

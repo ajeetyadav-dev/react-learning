b227b2b220c848b20a4ea6463669b9a88576b4aa
https://github.com/ajeetyadav0 <ay605403@gmail.com>
2026-06-28 20:52:11 +0530

    28

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,32 @@
 import { useState } from "react";
 function App(){
-    const [count,setCount] = useState(0);
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password,setPassword] = useState("");
+    const submit = (e) => {
+        e.preventDefault();
+        alert('Your Form Submit')
+    }
     return(
-        <>     
-           <h1>Counter : {count}</h1>
-        <button 
-        onClick={()=>
-                setCount(count + 1)}
-            
-        >+</button>
-        <button onClick={() =>{
-            if (count > 0){
-                setCount(count - 1);
-            }
-        }}>-</button>
-        <button onClick={() =>setCount(0)}>Reset</button>
-        <button onClick={() => setCount(count + 5)}>+5</button>
-        <hr></hr>
-        {count >=10 ? (
-            <h2 style={{ color : "green"}}>🎉Great Job!</h2>):(
-                <h2 style={{ color : "orange"}}>Keep Clicking.....</h2>
-            
-        )}
-           </>
-       
+        <>
+        <form onSubmit={submit}>
+       <input type="text" placeholder="Enter Your Name" value={name} 
+       onChange={(e) =>
+         setName(e.target.value)}/><br></br>
 
-    );
+         <input type="email" placeholder="Enter Your Email" value={email} 
+       onChange={(e) =>
+         setEmail(e.target.value)}/><br></br>
+
+         <input type="password" placeholder="Enter Your Password" value={password} 
+       onChange={(e) =>
+         setPassword(e.target.value)}/><br></br>
+         <button type="submit">Submit</button>
+</form>
+       <h2>Hello {name}</h2>
+       <h2> {email}</h2>
+       <h2> {password}</h2>
+</>
+    )
 }
 export default App;

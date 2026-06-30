7fdc23df1e198c606eb1c13cbcd924708b50a6b6
https://github.com/ajeetyadav0 <ay605403@gmail.com>
2026-06-30 23:32:33 +0530

    30

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,13 @@
 import { useState } from "react";
 function App(){
-    const [formData, setFormData] = 
-    useState({
-      name: "",
-      email:"",
-      password:"",
-    });
-   const handleChange = (e) => {const {name,value} = e.target;
-  setFormData({
-    ...formData,[name]:value
-  });
-  };
-    return(
-        <>
-       
-       <input type="text" placeholder="Enter Your Name" name="name" value={formData.name} 
-       onChange={handleChange}/><br></br><br></br>
-
-       <input type="email" name="email" placeholder="Enter Your Email" value={formData.email} onChange={handleChange}/><br></br><br></br>
-
-       <input type="password" name="password" placeholder="Enter Your Password" value={formData.password} onChange={handleChange}/><hr>
-       </hr>
-
-<h2>Name: {formData.name}</h2>
-<h2>Email: {formData.email}</h2>
-<h2>Password: {formData.password}</h2>
-
-      
-</>
-    )
+ const [loading]= useState(true);
+ return(
+  <>
+  {
+    loading ? <h1>Loading...</h1>:
+    <h1>Data Loaded✔️</h1>
+  }
+  </>
+ )
 }
 export default App;

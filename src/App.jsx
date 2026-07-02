cf10f93dd033e1d854ca69e5ae102eaed31cad49
https://github.com/ajeetyadav0 <ay605403@gmail.com>
2026-07-02 22:39:45 +0530

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,15 @@
-import { useState } from "react";
-function App() {
-  const [formData, setFormData] = useState({
-    email: "",
-    password: "",
-  });
-  const [error, setError] = useState("");
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
-  };
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      if (!formData.email.includes("@")) {
-        setError("Please enter a valid email address.");
-        return;
-      }
-      if (formData.password.length < 6) {
-        setError("Password must be at least 6 characters long.");
-        return;
-      }
-      setError("");
-      alert("Form submitted successfully!");
+import { useState , useEffect } from "react";
+function App(){
+ useEffect(()=>{
+  console.log("Mounted");
+  return ()=>{
+    console.log("Component Unmounted");
     };
-    return (
-      <div style={{ width: "350px", margin: "50px auto" }}>
-        <h1>Login Form</h1>
-        <form onSubmit={handleSubmit}>
-          <input type="email" name="email" placeholder="Enter Email" value={formData.email} onChange={handleChange} style={{ width: "100%", padding: "10px", marginBottom: "15px" }} />
-          <input type="password" name="password" placeholder="Enter Password" value={formData.password} onChange={handleChange} style={{ width: "100%", padding: "10px", marginBottom: "15px" }} />
-
-          {
-            error && <p style={{ color: "red" }}>{error}</p>
-
-          }
-          <button type="submit" style={{ padding: "10px 20px", cursor: "pointer" }}>Login</button>
-        </form>
-      </div>
-    );
-  }
-  export default App;
+}, []);
+return(
+  <>
+  <h1>Cleanup Function</h1>
+  </>
+);
+}
+export default App;
